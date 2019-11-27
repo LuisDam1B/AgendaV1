a6e43e5ebf4b2c0a67203d69fa0970dbdd2018f7
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-11-27 13:44:02 +0100

    Terminado reciclerView y Add floatingactionbutton V2

--- app/src/main/java/com/example/agendav1/Adaptador.java
+++ app/src/main/java/com/example/agendav1/Adaptador.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,38 +27,71 @@ public class Adaptador extends RecyclerView.Adapter implements View.OnClickListe
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.agenda,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.linear_recicleview_agenda,parent,false);
 
         view.setOnClickListener(this);
+        Holder holder = new Holder(view,context);
 
-        return null;
+
+
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((Holder)holder).binHolder(datos.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return datos.size();
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View view) {
+        if (listenerClick != null) listenerClick.onClick(view);
     }
 }
 
 class Holder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
+    ImageView imagenPerfil;
+    TextView nombre;
+    TextView apellidos;
+    TextView telefono;
+    TextView email;
 
-    public Holder(@NonNull View itemView) {
+    Persona persona;
+    IUsuario listener;
+    Context context;
+
+    public Holder(@NonNull View itemView, Context context)
+    {
         super(itemView);
+
+        imagenPerfil = itemView.findViewById(R.id.imagenPerfil);
+        nombre = itemView.findViewById(R.id.tvNombre);
+        apellidos = itemView.findViewById(R.id.tvApellidos);
+        telefono = itemView.findViewById(R.id.tvTelefono);
+        email = itemView.findViewById(R.id.tvEmail);
+        this.context = context;
     }
+
+    public void binHolder(Persona persona){
+        this.persona = persona;
+        nombre.setText(persona.getNombre());
+        apellidos.setText(persona.getApellidos());
+        telefono.setText(String.valueOf(persona.getTelefono()));
+        email.setText(persona.getEmail());
+    }
+
 
     @Override
     public void onClick(View v) {
+        if (listener != null){
+            listener.clickPersona(persona);
+        }
 
     }
 }
--- app/src/main/java/com/example/agendav1/MainActivity.java
+++ app/src/main/java/com/example/agendav1/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.agendav1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        final ArrayList<Persona> datos = new ArrayList<>();
+
+        datos.add(new Persona("Adrian","Gonzalvez Jordan",665200454,"adriangonzalves@gamil.com"));
+        datos.add(new Persona("Alberto","Garcia Belenger",665230454,"albertogarcia@gamil.com"));
+        datos.add(new Persona("Alejandro","Gonzalvez Jordan",665254353,"alejandorlinare@gamil.com"));
+        datos.add(new Persona("Carlos","Linares Muñoz",665234454,"carlosmarin@gamil.com"));
+        datos.add(new Persona("David","Collado",665243454,"davidcollado@gamil.com"));
+        datos.add(new Persona("Pepe","Pepito flo",665233454,"pepepeito@gamil.com"));
+        datos.add(new Persona("Juan","Juanito Sax",665202454,"juanjuanito@gamil.com"));
+        datos.add(new Persona("Cristiano","Ronaldo Lima",621334454,"cristianoronaldo@gamil.com"));
+        datos.add(new Persona("Leo","Messi Sanz",609480454,"leomessi@gamil.com"));
+
+        Adaptador adaptador = new Adaptador(datos,this);
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(adaptador);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,true));
     }
 }
